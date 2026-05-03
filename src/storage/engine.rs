fab04151c6e6ac7a8dc4c0e4fb12669e5ce93541
Chris Griffing <cmgriffing@gmail.com>
2026-05-02 17:03:02 -0700

    Revert "experiment(storage): inline get() and hoist memory_enabled check"
    This reverts commit 37f6bbc9c265f2d7fde96052b1b7fc10c29ac0c0.

--- src/storage/engine.rs
+++ src/storage/engine.rs
@@ -109,17 +109,16 @@ impl StorageEngine {
         }
     }
 
-    #[inline]
     pub fn get(&self, key: &str) -> Option<StoredValue> {
-        if !self.memory.is_enabled_sync() {
-            return self.data.get(key).map(|v| v.clone());
-        }
-
         let value = self.data.get(key).map(|v| v.clone());
+        
         if value.is_some() {
-            let rt = tokio::runtime::Handle::current();
-            rt.block_on(self.memory.record_read(key));
+            if self.memory.is_enabled_sync() {
+                let rt = tokio::runtime::Handle::current();
+                rt.block_on(self.memory.record_read(key));
+            }
         }
+        
         value
     }
 
